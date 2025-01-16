1340648c88244d3f21294b67e859f2b645a16ecf
IONUOHA <101448338+IONUOHA@users.noreply.github.com>
2025-01-16 22:19:27 +0100

    initial_commit

--- etl_pipeline/CapstoneETL.py
+++ etl_pipeline/CapstoneETL.py
@@ -3,24 +3,9 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
-
-def extract(**kwargs):
-    print("Extracting data...")
-    # Your extraction logic here
-nycpayroll_2021_df = pd.read_csv("nycpayroll_2021.csv")
-nycpayroll_2020_df = pd.read_csv("nycpayroll_2020.csv")
-empmaster_df = pd.read_csv("Empmaster.csv")
-TitleMaster_df = pd.read_csv("TitleMaster.csv")
-Agencymaster_df = pd.read_csv("AgencyMaster.csv")
-def transform(**kwargs):
-    print("Transforming data...")
-    # Your transformation logic here
-
-def load(**kwargs):
-    print("Loading data...")
-    # Your loading logic here
-
-
+from etl_pipeline.extract import extract_data
+from etl_pipeline.transform import transform_data
+from etl_pipeline.load import load_data
 ## setting default arguments
 default_args ={
     'owner': 'Ike_Onuoha',
@@ -41,20 +26,20 @@ with DAG(
 ) as dag:
    ##def task 1
    Extract_task = PythonOperator(
-      task_id = 'Extract'
-      python_callable=extract
+      task_id = 'Extract_data'
+      python_callable=extract_data
    )
 
    # def task 2
    Transform_task = PythonOperator(
-      task_id = 'Transform',
-      python_callable=transform
+      task_id = 'transform_data',
+      python_callable=transform_data
       bash_command = 'sleep 10'
    ) 
 
    #def task 3
    load_task = pythonOperator(
-      task_id = 'load',
+      task_id = 'load_data',
       python_callable=load
    )
 
